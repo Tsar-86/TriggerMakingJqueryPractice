6ec8c58701e490e960d2b5380005950236681d29
Tsarkov Kirill <Tsarkovkirill@mail.ru>
2024-02-27 23:53:54 +0300

    Add list in choose type distribution button and function to open this list

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let topTriggerButton = document.getElementById('top_trigger_button');
+let firstSectionButton = document.querySelector('.distribution_type_list');
+let firstSectionButtonList = document.querySelector('.first_section_button_list');
+
+firstSectionButton.addEventListener( 'click', () => {
+    firstSectionButton.style.margin = 0;
+    firstSectionButtonList.style.display = 'block';
+    firstSectionButtonList.style.flexDirection = 'columm';
+});
+
+
