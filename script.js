f22849b93206597a8fa0f24c7a23afb8a747062e
Tsarkov Kirill <Tsarkovkirill@mail.ru>
2024-03-04 23:08:34 +0300

    Add percentage distribution section

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let distributionTypeButton = document.querySelector('.distribution_type_list');
-let distributionTypeButtonList = document.querySelector('.distribution_type_button_list');
+// let distributionTypeButton = document.querySelector('.distribution_type_list');
+// let distributionTypeButtonList = document.querySelector('.distribution_type_button_list');
 
 let managersDistributionTypeButton = document.querySelector('.managers_distribution_list');
 let managersDistributionButtonList = document.querySelector('.managers_distribution_button_list');
 
-distributionTypeButton.addEventListener( 'click', () => {
-    distributionTypeButton.style.margin = 0;
-    distributionTypeButtonList.style.display = 'block';
-    distributionTypeButtonList.style.flexDirection = 'columm';
-});
+// distributionTypeButton.addEventListener( 'click', () => {
+//     distributionTypeButton.style.margin = 0;
+//     distributionTypeButtonList.style.display = 'block';
+//     distributionTypeButtonList.style.flexDirection = 'columm';
+// });
 
 
 managersDistributionTypeButton.addEventListener( 'click', () => {
